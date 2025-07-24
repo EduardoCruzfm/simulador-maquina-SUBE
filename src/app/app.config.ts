9e4ad1f25c8e21387b6032c14f741cd379dc0446
eduardo <eduardofrankcruzmendez@gmail.com>
2025-07-23 21:48:34 -0300

    feat: configuracion de providers e instalacion de packages

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,23 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { FIREBASE_OPTIONS } from '@angular/fire/compat';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideHttpClient, withFetch } from '@angular/common/http';
+import { environment } from '../environments/environment';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), 
+          provideRouter(routes), 
+          provideFirebaseApp(() => initializeApp(environment.firebaseConfig)), 
+          provideAuth(() => getAuth()), 
+          provideFirestore(() => getFirestore()), 
+          provideHttpClient(withFetch()),
+        { provide: FIREBASE_OPTIONS, useValue: environment.firebaseConfig }, provideAnimationsAsync(),
+        provideAnimations(),
+    ]
 };
